4091ea44b05efee1582c42a9d6ee17b503239cd3
Khadija <khadijabelkabous02@gmail.com>
2024-11-26 16:44:05 +0100

    l'ajout de fonction add_tache

--- main.c
+++ main.c
@@ -12,6 +12,30 @@ Tache taches[101];
 int nb_taches = 0;
 
 
+void add_tache() {
+        
+   if(nb_taches>=101){
+        printf("la liste est pleine");
+        return;
+    }
+    Tache nouvelle_tache;
+    printf("entrer le titre de tache:\n");
+    scanf(" %[^\n]",nouvelle_tache.titre);
+    printf("entrer la description:\n");
+    scanf(" %[^\n]", nouvelle_tache.description);
+    printf("entrer la date:\n");
+    scanf(" %[^\n]", nouvelle_tache.date);
+    printf("entrer la priorité(low / high):\n");
+    scanf(" %[^\n]", nouvelle_tache.priorite);
+    taches[nb_taches] = nouvelle_tache;
+    nb_taches++;
+        printf("Tâche ajoutée avec succès!\n");
+    }
+    
+   
+ 
+
+
 int main()
 {
     
@@ -26,7 +50,26 @@ do{
         printf("Choisissez une option: ");
         scanf("%d", &choix);
 
-  
+  switch (choix) {
+            case 1:
+                add_tache();
+            break;
+            case 2:
+                
+            break;
+            case 3:
+                
+            break;
+            case 4:
+                
+            break;
+            case 5:
+                
+            break;
+    default:
+    printf("choix invalide. veuillez reessayer. \n");
+}
+    
    
     
 }while(choix!=6);
