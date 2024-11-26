3ab03cba73ee12655f5cc1d3eae35ed8c225e9e8
Khadija <khadijabelkabous02@gmail.com>
2024-11-26 16:39:51 +0100

    ajout de srtucture et menu

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+    char titre[101];
+    char description[1000];
+    char date[12];
+    char priorite[11];//high or low
+} Tache;
+
+Tache taches[101];
+int nb_taches = 0;
+
+
 int main()
 {
     
+  int choix;
+do{
+     printf(" Menu de Gestion des Tâches\n ");
+        printf("1. Ajouter une Tâche\n");
+        printf("2. Afficher la Liste des Tâches\n");
+        printf("3. Modifier une Tâche\n");
+        printf("4. Supprimer une Tâche\n");
+        printf("5. Filtrer les Tâches par Priorité\n");
+        printf("Choisissez une option: ");
+        scanf("%d", &choix);
+
+  
+   
+    
+}while(choix!=6);
+
+return 0;
+
 }
